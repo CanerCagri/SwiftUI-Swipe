0dde8b9f092d93584b151a8e5f8cc79c25e34e83
CanerCagri <canerrcagrii@gmail.com>
2023-07-11 16:59:41 +0300

    Initial Commit

---
+++ SwiftUI-Swipe/SwiftUI_SwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SwipeApp.swift
+//  SwiftUI-Swipe
+//
+//  Created by Caner Çağrı on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
